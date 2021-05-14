07061f6824d501fcd97de61aa88f53c866b79b0a
dave vebber <davevebber@gmail.com>
2021-05-14 00:37:43 -0700

    apollo server set up and queries for thoughts and users working

--- server/server.js
+++ server/server.js
@@ -18,6 +18,6 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphyQL at http://localhost:${PORT}${server.graphqlPath}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
   });
 });
